581c78b7712eb91e420cf81114cf468aa2c9b7e5
prekshya-dawadi <prekshya019@gmail.com>
2023-11-28 14:39:33 +0545

    RotateOnClick

--- Assets/Scripts/flip.cs
+++ Assets/Scripts/flip.cs
@@ -6,17 +6,28 @@ using System;
 
 public class flip : MonoBehaviour
 {
+    [SerializeField]
+    Button startBtn;
     private Vector3 rotationVector;
+    bool startBtnClicked = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (startBtn != null){
+            startBtn.onClick.AddListener(()=>startBtn_Click());
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (startBtnClicked = true){
+            transform.Rotate(rotationVector*Time.deltaTime);    
+        }
+    }
+
+    void startBtn_Click(){
+        startBtnClicked = true;
         rotationVector = new Vector3(0,90,0);
-        transform.Rotate(rotationVector*Time.deltaTime);    
     }
 }
