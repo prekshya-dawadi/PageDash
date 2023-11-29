f60bcf9c5c052658f907eaaf381b37ba0459b3f2
prekshya-dawadi <prekshya019@gmail.com>
2023-11-29 20:32:01 +0545

    Added a start scene, and object rotates on space down

--- Assets/Scripts/flip.cs
+++ Assets/Scripts/flip.cs
@@ -7,27 +7,30 @@ using System;
 public class flip : MonoBehaviour
 {
     [SerializeField]
-    Button startBtn;
+
     private Vector3 rotationVector;
-    bool startBtnClicked = false;
+    private bool spaceDown = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        if (startBtn != null){
-            startBtn.onClick.AddListener(()=>startBtn_Click());
-        }
+ 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (startBtnClicked = true){
+        if (Input.GetKey("space")){
+            space_down();
+        }
+        
+        if (spaceDown == true){
             transform.Rotate(rotationVector*Time.deltaTime);    
         }
     }
 
-    void startBtn_Click(){
-        startBtnClicked = true;
+    void space_down(){
+        spaceDown = true;
         rotationVector = new Vector3(0,90,0);
     }
 }
