40bf48af4de4a5f22531dbbd2b9bef9b475d8297
prekshya-dawadi <prekshya019@gmail.com>
2023-12-03 09:05:27 +0545

    basic character control

--- Assets/Scripts/characterControl.cs
+++ Assets/Scripts/characterControl.cs
@@ -11,6 +11,17 @@ public class characterControl : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKey("up")){
+            GetComponent<Rigidbody>().velocity = new Vector3(0,0,2);
+        }
+        if (Input.GetKey("down")){
+            GetComponent<Rigidbody>().velocity = new Vector3(0,0,-2);
+        }
+        if (Input.GetKey("right")){
+            GetComponent<Rigidbody>().velocity = new Vector3(2,0,0);
+        }
+        if (Input.GetKey("left")){
+            GetComponent<Rigidbody>().velocity = new Vector3(-2,0,0);
+        }
     }
 }
